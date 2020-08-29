5a7712a2e7081bb2d2b235edc62d830989b0d1dc
e5c0b <e5c0b@ugrad.cs.ubc.ca>
2020-08-28 22:00:37 -0700

    tin-tsk replaced with sloppy

--- README.md
+++ README.md
@@ -20,8 +20,17 @@ Author: kenhyj
 
 ### Commands:
 
-#### Text Commands
+#### Text Reply
+
+Upper-lower cases have been made negligible. Spelling however matters.
 
 - _!babyshark_ : answers with "Baby shark, doo doo doo doo doo doo~ Baby shark, doo doo doo doo doo doo \n !play baby shark"
 - _!fliptable_ : answers with "┛◉Д◉)┛ 彡 ┻━┻"
 - _!transformers_ : answers with "robots in disguise"
+- _!drtin~_ : answers with "Good enough for me♡"
+
+#### Embed Reply
+
+Upper-lower cases have been made negligible. Spelling however matters.
+
+- _!sloppy_ _@username_: answers with Sloppy Sloppy Sloppy embed with the mentioned user's username and avatar. If the username is invalid or empty, it will default to the author of this message.
--- bot.js
+++ bot.js
@@ -5,29 +5,72 @@
 // Run dotenv
 require('dotenv').config();
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
+// const Discord = require('discord.js');
+// const client = new Discord.Client();
+// const MessageEmbed = new Discord.MessageEmbed();
+const { Client, MessageEmbed } = require('discord.js');
+const client = new Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+const PREFIX = '!';
+
 // Add list of text memes here
-const messages = {
+const texts = {
   fliptable: '(┛◉Д◉)┛彡┻━┻',
   babyshark:
     '♫ Baby shark, doo doo doo doo doo doo~ ♪ Baby shark, doo doo doo doo doo doo \n !play baby shark',
   transformers: '♪robots in disguise~',
   'drtin~': 'Good enough for me♡',
-  'tin-tsk': 'Sloppy Sloppy Sloppy',
+  pepewhy: 'https://tenor.com/view/pepe-why-pepe-pepehands-crying-gif-12683546',
 };
 
+// Add list of gifs of attachments memes here:
+const gifs = {
+  pepecry: 'https://tenor.com/view/pepe-why-pepe-pepehands-crying-gif-12683546',
+  licky: 'https://tenor.com/view/lick-lips-oreimo-anime-gif-12850352',
+  shame: 'https://tenor.com/view/shame-game-of-thrones-gif-5547122',
+};
+// Add list of static picture memes here:
+const staticpics = {};
+
 // this will return the text memes listed above.
-// you must have ! in front of the command
-client.on('message', (msg) => {
-  for (const key in messages) {
-    if (msg.content === '!'.concat(key)) {
-      msg.reply(messages[key]);
+// you must have the PREFIX in front of the command
+client.on('message', (message) => {
+  // terminates if PREFIX isn't in the beginning. it's faster with this on there.
+  if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+  // this is exclusively for the use of sloppy meme
+  if (message.content.toLowerCase().startsWith(PREFIX + 'sloppy')) {
+    const user = message.mentions.users.first() || message.author;
+    const avatarEmbed = new MessageEmbed()
+      .setColor(0xffffff)
+      .setTitle('Sloppy Sloppy Sloppy')
+      .setAuthor('@' + user.username)
+      .setImage(user.avatarURL())
+      .setFooter('from ' + message.author.username);
+    message.channel.send(avatarEmbed);
+    return; // reduces slowness
+  }
+  for (const key in texts) {
+    if (message.content.toLowerCase() === PREFIX + key) {
+      message.reply(texts[key]);
+      return; // reduces slowness
+    }
+  }
+  for (const kay in gifs) {
+    if (message.content.toLowerCase() === PREFIX + kay) {
+      console.log('typeof message', typeof message);
+      console.log('typeof message.content', typeof message.content);
+      const embed = new MessageEmbed()
+        .setTitle('A slick little embed')
+        .setColor(0xff0000)
+        .setDescription('Hello, this is a slick embed!')
+        // .setImage(message.author.avatarURL());
+        .setImage(message.author.avatarURL());
+      message.channel.send(embed);
+      return;
     }
   }
 });
