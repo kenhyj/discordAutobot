d9adf40c18ef9aea06e525a56b3f6606bf7fd699
e5c0b <e5c0b@ugrad.cs.ubc.ca>
2020-08-31 21:47:57 -0700

    among us gifs

--- bot.js
+++ bot.js
@@ -40,6 +40,21 @@ const directedmeme = {
     title: 'Game of Thrones. Shame',
     src: 'https://media.giphy.com/media/vX9WcCiWwUF7G/source.gif',
   },
+  amongkill1: {
+    title: 'Among Us. Kill. Stab',
+    src:
+      'https://media1.tenor.com/images/3cedee4f8118855c83ea05463498f326/tenor.gif',
+  },
+  amongkill2: {
+    title: 'Among Us. Kill. Shot',
+    src:
+      'https://media1.tenor.com/images/a0d13ec25f9774f155b6cd5ebf12a6c8/tenor.gif',
+  },
+  amongkill3: {
+    title: 'Among Us. Kill. Snap',
+    src:
+      'https://media1.tenor.com/images/ef4993b593954811a0c0a1c98af698a3/tenor.gif',
+  },
 };
 
 // this will return the text memes listed above.
