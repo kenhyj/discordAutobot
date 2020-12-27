5821f986d8b420e011feb69fba1516c14ac692c1
e5c0b <e5c0b@ugrad.cs.ubc.ca>
2020-12-26 20:18:08 -0800

    git rule update

--- commands/rules.js
+++ commands/rules.js
@@ -1,5 +1,5 @@
 const rules =
-  '\n-No Profanity / Slurs\n-Be respectful to others\n-Keep it Drama Free\n-Avoid controversial/political topics\n-Have a good time\n-Do not spam for shout outs or followers during live stream!\n';
+  '\n• No excessive Profanity / Slurs\n• Zero tolerance for NSFW / NSFL content / discussions. (includes usernames and avatars)\n• Be respectful to others\n• No Advertisements or promotions of any kind in this server or in DMs, this includes Discord servers, social media, content channels etc\n• English is the main language in this server - please refrain from using other languages\n• No bullying\n• Keep it Drama Free\n• Avoid controversial/political topics\n• Use Common Sense.\n';
 
 module.exports = {
   // the command recognized
